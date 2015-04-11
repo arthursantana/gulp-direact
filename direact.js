12989a9748e2269ccdde5c937b4f93bb8f1076b3
Arthur Santana <rockslave@gmail.com>
2015-04-11 18:41:35 -0300

    using list of valid HTML5 tags

--- direact.js
+++ direact.js
@@ -7,6 +7,8 @@ var result;
 var counter = 0;
 var components = [];
 
+var tags = ["!DOCTYPE", "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"];
+
 var a2s = function (attribs) {
    attributeStrings = "";
    for (var key in attribs) {
@@ -16,7 +18,7 @@ var a2s = function (attribs) {
 };
 
 function isComponent(string) {
-   return string[0] >= 'A' && string[0] <= 'Z'
+   return (tags.indexOf(string) == -1)
 };
 
 var toBuf = function (string) {
