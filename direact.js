11a3f7a7d923a4d90e0a771f04777159384e7021
Arthur Santana <rockslave@gmail.com>
2015-04-16 06:13:48 -0300

    lowercasing tags automatically

--- direact.js
+++ direact.js
@@ -7,7 +7,7 @@ var result;
 var counter = 0;
 var components = [];
 
-var tags = ["!DOCTYPE", "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"];
+var tags = ["!doctype", "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr"];
 
 var a2s = function (attribs) {
    attributeStrings = "";
@@ -18,7 +18,7 @@ var a2s = function (attribs) {
 };
 
 function isComponent(string) {
-   return (tags.indexOf(string) == -1)
+   return (tags.indexOf(string.toLowerCase()) == -1)
 };
 
 var toBuf = function (string) {
@@ -28,12 +28,14 @@ var toBuf = function (string) {
 var parser = new htmlparser.Parser({
    onopentag: function(tagname, attribs){
       if(isComponent(tagname)){
+         console.log(tagname);
          var id = "ReactComponent" + counter; counter++;
          components.push({id: id, name: tagname, att: attribs});
          toBuf("<div id=\"" + id + "\"></div>");
       }
       else {
-         toBuf("<" + tagname + a2s(attribs) + ">");
+         console.log(tagname.toLowerCase());
+         toBuf("<" + tagname.toLowerCase() + a2s(attribs) + ">");
       }
    },
 
